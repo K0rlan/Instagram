91effd429e26f98b631a8481af443465d8b5059c
Korlan Omarova <omar_korlan@mail.ru>
2021-01-31 06:18:56 +0600

    collectionView

--- Instagram/ViewController.swift
+++ Instagram/ViewController.swift
@@ -11,11 +11,18 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var customNavigationBar: NavigationBarView!
     
+    @IBOutlet weak var collectionView: UICollectionView!
     var stories = [Stories(name: "Koko", image: "avatar1"), Stories(name: "Koko", image: "ava1"), Stories(name: "Koko", image: "ava1"), Stories(name: "Koko", image: "ava1"), Stories(name: "Koko", image: "ava1"), Stories(name: "Koko", image: "ava1"), Stories(name: "Koko", image: "ava1"), Stories(name: "Koko", image: "ava1")]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         customNavigationBar.delegate = self
+        
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        collectionView.showsHorizontalScrollIndicator = false
+
+        
     }
 
 
@@ -28,4 +35,19 @@ extension ViewController: NavBarDelegate{
         print("Direct Button Tapped")
     }
    
+}
+
+extension ViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+   
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        stories.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! StoriesCollectionViewCell
+        let story = stories[indexPath.row]
+        cell.imageView.image = UIImage(named: story.image)
+        cell.textLabel.text = story.name
+        return cell
+    }
 }
